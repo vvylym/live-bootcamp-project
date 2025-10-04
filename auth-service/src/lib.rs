560fbb34a979aafc665e79b4a40d04dd1348b6df
vvylym <235853469+vvylym@users.noreply.github.com>
2025-10-04 17:30:15 +0100

    [Sprint 1][Task 6] chore: update dependencies and add reqwest for testing

--- auth-service/src/lib.rs
+++ auth-service/src/lib.rs
@@ -6,12 +6,13 @@
 //! The `Application` struct provides methods to build and run the service.
 //! It uses the `axum` framework for routing and handling HTTP requests.
 use axum::{serve::Serve, response::Html, routing::get, Router};
+use tokio::net::TcpListener;
 use tower_http::services::ServeDir;
 
 // This struct encapsulates our application-related logic.
 pub struct Application {
     /// The axum server instance.
-    server: Serve<Router, Router>,
+    server: Serve<TcpListener, Router, Router>,
     // address is exposed as a public field
     // so we have access to it in tests.
     pub address: String,
@@ -24,10 +25,10 @@ impl Application {
         // Also, remove the `hello` route.
         // We don't need it at this point!
         let router = Router::new()
-            .nest_service("/", ServeDir::new("assets"))
+            .route_service("/", ServeDir::new("assets"))
             .route("/hello", get(hello_handler));
 
-        let listener = tokio::net::TcpListener::bind(address).await?;
+        let listener = TcpListener::bind(address).await?;
         let address = listener.local_addr()?.to_string();
         let server = axum::serve(listener, router);
 
