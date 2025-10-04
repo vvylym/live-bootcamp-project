9b08090240f5d3f47ae986870e23217b52478e12
vvylym <235853469+vvylym@users.noreply.github.com>
2025-10-04 17:38:14 +0100

    [Sprint 1][Task 7] test: add helper for testing auth service

---
+++ auth-service/tests/api/helpers.rs
@@ -0,0 +1,89 @@
+use auth_service::Application;
+use reqwest::{Client, ClientBuilder};
+
+/// A helper struct to spawn and interact with a test instance of our application.
+pub struct TestApp {
+    /// The address of the running instance of our application.
+    pub address: String,
+    /// The HTTP client to interact with the application.
+    pub http_client: reqwest::Client,
+}
+
+impl TestApp {
+    /// Spawns a new instance of our application and returns a `TestApp` instance.
+    pub async fn new() -> Self {
+        let app = Application::build("127.0.0.1:0")
+            .await
+            .expect("Failed to build app");
+
+        let address = format!("http://{}", app.address.clone());
+
+        // Run the auth service in a separate async task
+        // to avoid blocking the main test thread. 
+        #[allow(clippy::let_underscore_future)]
+        let _ = tokio::spawn(app.run());
+
+        // Create a Reqwest http client instance
+        let http_client = Client::builder().build().unwrap(); 
+
+        // Create new `TestApp` instance and return it
+        Self {
+            address,
+            http_client,
+        }
+    }
+
+    /// Sends a GET request to the root endpoint ("/") of the application.
+    pub async fn get_root(&self) -> reqwest::Response {
+        self.http_client
+            .get(&format!("{}/", &self.address))
+            .send()
+            .await
+            .expect("Failed to execute request.")
+    }
+
+    /// Sends a POST request to the "/signup" endpoint of the application.
+    pub async fn signup(&self) -> reqwest::Response {
+        self.http_client
+            .post(&format!("{}/signup", &self.address))
+            .send()
+            .await
+            .expect("Failed to execute request.")
+    }
+
+    /// Sends a POST request to the "/login" endpoint of the application.
+    pub async fn login(&self) -> reqwest::Response {
+        self.http_client
+            .post(&format!("{}/login", &self.address))
+            .send()
+            .await
+            .expect("Failed to execute request.")
+    }
+
+    /// Sends a POST request to the "/logout" endpoint of the application.
+    pub async fn logout(&self) -> reqwest::Response {
+        self.http_client
+            .post(&format!("{}/logout", &self.address))
+            .send()
+            .await
+            .expect("Failed to execute request.")
+    }
+
+    /// Sends a POST request to the "/request-2fa" endpoint of the application.
+    pub async fn verify_2fa(&self) -> reqwest::Response {
+        self.http_client
+            .post(&format!("{}/verify-2fa", &self.address))
+            .send()
+            .await
+            .expect("Failed to execute request.")
+    }
+
+    /// Sends a POST request to the "/verify-token" endpoint of the application.
+    pub async fn verify_token(&self) -> reqwest::Response {
+        self.http_client
+            .post(&format!("{}/verify-token", &self.address))
+            .send()
+            .await
+            .expect("Failed to execute request.")
+    }
+}
---
+++ auth-service/tests/api/routes.rs
@@ -0,0 +1,15 @@
+use crate::helpers::TestApp;
+
+// Tokio's test macro is used to run the test in an async environment
+#[tokio::test]
+async fn root_returns_auth_ui() {
+    let app = TestApp::new().await;
+
+    let response = app.get_root().await;
+
+    assert_eq!(response.status().as_u16(), 200);
+    assert_eq!(response.headers().get("content-type").unwrap(), "text/html");
+}
+
+// TODO: Implement tests for all other routes (signup, login, logout, verify-2fa, and verify-token)
+// For now, simply assert that each route returns a 200 HTTP status code.
