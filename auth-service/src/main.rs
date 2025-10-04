5e1ae7492624e4d0c3fd20e99af1106df5fd31c6
aura-ai <26940013-aura-ai@users.noreply.gitlab.com>
2025-10-04 14:32:49 +0100

    [Sprint 1][Task 2] feat(auth): change message from hello handler

--- auth-service/src/main.rs
+++ auth-service/src/main.rs
@@ -17,6 +17,5 @@ async fn main() {
 }
 
 async fn hello_handler() -> Html<&'static str> {
-    // TODO: Update this to a custom message!
-    Html("<h1>Hello, World!</h1>")
+    Html("<h1>Auth Service</h1><br/><p>More coming soon...</p>")
 }
